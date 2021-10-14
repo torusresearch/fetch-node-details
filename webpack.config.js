f6a97db51cc4a45e22322f45c989e2b44787a906
chaitanyapotti <chaitanya.potti@gmail.com>
2021-10-14 16:12:58 +0800

    Add a rollup build

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,8 @@ const { NODE_ENV = "production" } = process.env;
 
 const baseConfig = {
   mode: NODE_ENV,
-  devtool: NODE_ENV === "production" ? false : "source-map",
-  entry: "./index.ts",
+  devtool: "source-map",
+  entry: "./src/index.ts",
   target: "web",
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -42,25 +42,11 @@ const optimization = {
   },
 };
 
-const babelLoaderWithPolyfills = {
+const babelLoader = {
   test: /\.(ts|js)x?$/,
   exclude: /(node_modules|bower_components)/,
   use: {
     loader: "babel-loader",
-  },
-};
-
-const babelLoader = { ...babelLoaderWithPolyfills, use: { loader: "babel-loader", options: { plugins: ["@babel/transform-runtime"] } } };
-
-const umdPolyfilledConfig = {
-  ...baseConfig,
-  output: {
-    ...baseConfig.output,
-    filename: `${pkgName}.polyfill.umd.min.js`,
-    libraryTarget: "umd",
-  },
-  module: {
-    rules: [babelLoaderWithPolyfills],
   },
 };
 
@@ -124,7 +110,7 @@ const nodeConfig = {
   target: "node",
 };
 
-module.exports = [umdPolyfilledConfig, umdConfig, cjsConfig, cjsBundledConfig, nodeConfig];
+module.exports = [umdConfig, cjsConfig, cjsBundledConfig, nodeConfig];
 // module.exports = [cjsConfig];
 
 // v5
