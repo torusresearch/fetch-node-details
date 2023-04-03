cc208e6010bbe3fbcb2f8ab2aa9006eacc40aabf
himanshuchawla009 <himanshuchawla2014@gmail.com>
2023-04-03 14:17:41 +0530

    remove redis pwd

--- packages/fnd-server/src/database/redis.ts
+++ packages/fnd-server/src/database/redis.ts
@@ -1,7 +1,7 @@
 import log from "loglevel";
 import { createClient } from "redis";
 
-const { REDIS_HOSTNAME, REDIS_PORT, REDIS_PASSWORD } = process.env;
+const { REDIS_HOSTNAME, REDIS_PORT } = process.env;
 
 if (!REDIS_HOSTNAME) {
   throw new Error("Please pass REDIS_HOSTNAME in env variables");
@@ -10,7 +10,7 @@ const [host, port] = REDIS_HOSTNAME.split(":");
 
 log.info("Redis", host, port, REDIS_PORT);
 
-const client = createClient({ socket: { host, port: Number(port || REDIS_PORT) }, password: REDIS_PASSWORD });
+const client = createClient({ socket: { host, port: Number(port || REDIS_PORT) } });
 
 client.connect();
 
