452dcf822e0b2e4a69f51ff06cbce07f7a70b8a8
himanshuchawla009 <himanshuchawla2014@gmail.com>
2023-04-18 11:35:31 +0530

    push to ecr only when test passes

--- ci.sh
+++ ci.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-./node_modules/.bin/wait-port 8060
+/node_modules/.bin/wait-port 8060
 sleep 10
-./node_modules/.bin/mocha ./packages/fetch-node-details/test/nodeDetail.test.ts
+/node_modules/.bin/mocha ./packages/fetch-node-details/test/nodeDetail.test.ts
