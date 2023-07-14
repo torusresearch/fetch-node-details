aac1c79240e131109d76b5cf5c425eab1095d83b
chaitanyapotti <chaitanya.potti@gmail.com>
2023-07-14 17:25:06 +0800

    update binance node

--- packages/fnd-base/src/legacyMainnetConfig.ts
+++ packages/fnd-base/src/legacyMainnetConfig.ts
@@ -11,7 +11,7 @@ export const NODE_DETAILS_MAINNET: INodeDetails = {
     "https://torus-mainnet.cosmos.network/jrpc",
     "https://torus2.etherscan.com/jrpc",
     "https://torus-node-v2.skalelabs.com/jrpc",
-    "https://torus-node.binancex.dev/jrpc",
+    "https://binancex.tor.us/jrpc",
     "https://torusnode.ont.io/jrpc",
   ],
   torusIndexes: [1, 2, 3, 4, 5, 6, 7, 8, 9],
