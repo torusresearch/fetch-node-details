08f9286284fe74e00051ccd7489b4e829ecc36ad
himanshu <himanshuchawla009>
2024-04-29 19:19:02 +0530

    switch celeste endpoints

--- packages/fetch-node-details/test/config.ts
+++ packages/fetch-node-details/test/config.ts
@@ -1,33 +1,51 @@
 export const nodeDetailsCeleste = {
   currentEpoch: "1",
   torusNodeEndpoints: [
-    "https://node-1.cluster-6.celeste.web3auth.io/jrpc",
-    "https://node-2.cluster-6.celeste.web3auth.io/jrpc",
-    "https://node-3.cluster-6.celeste.web3auth.io/jrpc",
-    "https://node-4.cluster-6.celeste.web3auth.io/jrpc",
-    "https://node-5.cluster-6.celeste.web3auth.io/jrpc",
+    "https://node-1.node.web3auth.io/sss/celeste/jrpc",
+    "https://node-2.node.web3auth.io/sss/celeste/jrpc",
+    "https://node-3.node.web3auth.io/sss/celeste/jrpc",
+    "https://node-4.node.web3auth.io/sss/celeste/jrpc",
+    "https://node-5.node.web3auth.io/sss/celeste/jrpc",
   ],
-  torusNodePub: [
-    {
-      X: "184a61fc08255c33ac8d57da86d957725e45157a4dfe6b80838b9c0acf450133",
-      Y: "06b2189341bc8ba97cd849f58bac8d6fa4484c42d57a529797a4d615bf20fbcd",
-    },
-    {
-      X: "3ca052df886a84096c2b2eded87cbfacab06d5e231b75a0fe8b6bc7fa4ed04a1",
-      Y: "1931cb683ff4c8216523f93b72f536dbbfae2c747ef40c48e0676641caf80d2b",
-    },
-    {
-      X: "498636b9bf53a0cfc337e23f506b1db5ebe55405666a412e23fc3ac5b289450b",
-      Y: "10dfdc3f72111c959b48b8caf2e42cfb907406b3efceb3d4d7b191a003094519",
-    },
-    {
-      X: "cd6541b449452f607705b0687b9aaaf3d3bd02bd615a5a80bfcb12201e12ade9",
-      Y: "3c1ef1d370c1dff494ae6360093e77df055b744dcf146d8b47ecb6dfd163d5af",
-    },
-    {
-      X: "7cb92d08513fca39e09b22d6d9ee5cc4e8ad782126067e0fb72c4717209fad3b",
-      Y: "b4c1fd6cb8beae828d6318cc7b085c2d80bdc51b46c086147d1537dab8ebae9b",
-    },
+  torusNodeSSSEndpoints: [
+    "https://node-1.node.web3auth.io/sss/celeste/jrpc",
+    "https://node-2.node.web3auth.io/sss/celeste/jrpc",
+    "https://node-3.node.web3auth.io/sss/celeste/jrpc",
+    "https://node-4.node.web3auth.io/sss/celeste/jrpc",
+    "https://node-5.node.web3auth.io/sss/celeste/jrpc",
+  ],
+  torusNodeRSSEndpoints: [
+    "https://node-1.node.web3auth.io/rss/celeste",
+    "https://node-2.node.web3auth.io/rss/celeste",
+    "https://node-3.node.web3auth.io/rss/celeste",
+    "https://node-4.node.web3auth.io/rss/celeste",
+    "https://node-5.node.web3auth.io/rss/celeste",
+  ],
+  torusNodeTSSEndpoints: [
+    "https://node-1.node.web3auth.io/tss/celeste",
+    "https://node-2.node.web3auth.io/tss/celeste",
+    "https://node-3.node.web3auth.io/tss/celeste",
+    "https://node-4.node.web3auth.io/tss/celeste",
+    "https://node-5.node.web3auth.io/tss/celeste",
   ],
   torusIndexes: [1, 2, 3, 4, 5],
+  torusNodePub: [
+    {
+      X: "e0925898fee0e9e941fdca7ee88deec99939ae9407e923535c4d4a3a3ff8b052",
+      Y: "54b9fea924e3f3e40791f9987f4234ae4222412d65b74068032fa5d8b63375c1",
+    },
+    {
+      X: "9124cf1e280aab32ba50dffd2de81cecabc13d82d2c1fe9de82f3b3523f9b637",
+      Y: "fca939a1ceb42ce745c55b21ef094f543b457630cb63a94ef4f1afeee2b1f107",
+    },
+    {
+      X: "555f681a63d469cc6c3a58a97e29ebd277425f0e6159708e7c7bf05f18f89476",
+      Y: "606f2bcc0884fa5b64366fc3e8362e4939841b56acd60d5f4553cf36b891ac4e",
+    },
+    { X: "2b5f58d8e340f1ab922e89b3a69a68930edfe51364644a456335e179bc130128", Y: "4b4daa05939426e3cbe7d08f0e773d2bf36f64c00d04620ee6df2a7af4d2247" },
+    {
+      X: "3ecbb6a68afe72cf34ec6c0a12b5cb78a0d2e83ba402983b6adbc5f36219861a",
+      Y: "dc1031c5cc8f0472bd521a62a64ebca9e163902c247bf05937daf4ae835091e4",
+    },
+  ],
 };
