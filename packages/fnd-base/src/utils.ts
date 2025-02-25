561b801afb20d711a309a10745bc815626eaa8e7
lwin <lwin@tor.us>
2025-02-25 15:19:30 +0800

    refactor: validates key type and sig type for TSS Endpoints

--- packages/fnd-base/src/utils.ts
+++ packages/fnd-base/src/utils.ts
@@ -1,6 +1,8 @@
 import {
   INodeDetails,
+  KEY_TYPE,
   LEGACY_NETWORKS_ROUTE_MAP,
+  SIG_TYPE,
   TORUS_LEGACY_NETWORK,
   TORUS_LEGACY_NETWORK_TYPE,
   TORUS_NETWORK_TYPE,
@@ -11,6 +13,24 @@ import {
 } from "@toruslabs/constants";
 
 import { getSapphireNodeDetails } from "./sapphireNetworkConfig";
+
+export function validateKeyTypeAndSigTypeForTSS(keyType: WEB3AUTH_KEY_TYPE, sigType: WEB3AUTH_SIG_TYPE) {
+  if (sigType === SIG_TYPE.ECDSA_SECP256K1) {
+    if (keyType !== KEY_TYPE.SECP256K1) {
+      throw new Error("Invalid key type for ecdsa-secp256k1");
+    }
+  } else if (sigType === SIG_TYPE.BIP340) {
+    if (keyType !== KEY_TYPE.SECP256K1) {
+      throw new Error("Invalid key type for bip340");
+    }
+  } else if (sigType === SIG_TYPE.ED25519) {
+    if (keyType !== KEY_TYPE.ED25519) {
+      throw new Error("Invalid key type for ed25519");
+    }
+  } else {
+    throw new Error(`Invalid signature type, ${sigType}`);
+  }
+}
 
 export function fetchLocalConfig(network: TORUS_NETWORK_TYPE, keyType: WEB3AUTH_KEY_TYPE, sigType?: WEB3AUTH_SIG_TYPE): INodeDetails | undefined {
   if (Object.values(TORUS_SAPPHIRE_NETWORK).includes(network as TORUS_SAPPHIRE_NETWORK_TYPE)) {
